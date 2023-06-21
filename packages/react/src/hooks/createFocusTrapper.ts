d1ce7650b521a85aaee0102df06fdc9178ed20cb
Niko Helle <niko.hele@hyperactive.fi>
2023-06-21 13:48:35 +0300

    Focus trapper hook Sometimes focus must be trapped inside dialog etc. Or focus events of a group of elements must be controlled.
    This hook tracks and traps (if needed) focus between two elements.

---
+++ packages/react/src/hooks/createFocusTrapper.ts
@@ -0,0 +1,99 @@
+export type FocusTrapper = ReturnType<typeof createFocusTrapper>;
+export type FocusTrapperProps = {
+  firstTrapperElement?: HTMLElement;
+  lastTrapperElement?: HTMLElement;
+  onFocus?: (type: 'first' | 'last') => void;
+  trapIn?: boolean;
+  manualControls?: boolean;
+};
+export type Position = 'first' | 'last';
+export function createFocusTrapper({
+  firstTrapperElement,
+  lastTrapperElement,
+  onFocus,
+  trapIn = false,
+  manualControls = false,
+}: FocusTrapperProps) {
+  const elements = {
+    first: firstTrapperElement,
+    last: lastTrapperElement,
+  };
+  const disableElementFocus = (element?: HTMLElement) => {
+    if (!element) {
+      return;
+    }
+    element.setAttribute('tabindex', '-1');
+    element.setAttribute('disabled', 'true');
+  };
+  const enableElementFocus = (element?: HTMLElement) => {
+    if (!element) {
+      return;
+    }
+    element.setAttribute('tabindex', '0');
+    element.removeAttribute('disabled');
+  };
+  const disableTracking = () => {
+    disableElementFocus(elements.first);
+    disableElementFocus(elements.last);
+  };
+  const enableTracking = () => {
+    enableElementFocus(elements.first);
+    enableElementFocus(elements.last);
+  };
+  const getElementPosition = (element?: HTMLElement | Node | EventTarget | null): Position | undefined => {
+    if (!element) {
+      return undefined;
+    }
+    if (element === elements.first) {
+      return 'first';
+    }
+    if (element === elements.last) {
+      return 'last';
+    }
+    return undefined;
+  };
+  const listener = (focusEvent: FocusEvent) => {
+    const position = getElementPosition(focusEvent.target as HTMLElement);
+    if (!position) {
+      return;
+    }
+    if (onFocus) {
+      onFocus(position);
+    }
+    if (!trapIn && !manualControls) {
+      disableTracking();
+    }
+  };
+  const addFocusListener = (element?: HTMLElement) => {
+    if (!element) {
+      return;
+    }
+    element.addEventListener('focus', listener);
+  };
+  const removeFocusListener = (element?: HTMLElement) => {
+    if (!element) {
+      return;
+    }
+    element.removeEventListener('focus', listener);
+  };
+
+  addFocusListener(elements.first);
+  addFocusListener(elements.last);
+
+  return {
+    registerTrackerElement: (type: 'first' | 'last', element: HTMLElement | null) => {
+      removeFocusListener(elements[type]);
+      if (element) {
+        elements[type] = element;
+        addFocusListener(elements[type]);
+      }
+    },
+    dispose: () => {
+      removeFocusListener(elements.first);
+      removeFocusListener(elements.last);
+    },
+    disableTracking,
+    enableTracking,
+    getElementPosition,
+  };
+}
---
+++ packages/react/src/hooks/useFocusTrapping.ts
@@ -0,0 +1,70 @@
+import React, { useCallback, useEffect, useRef } from 'react';
+
+import { FocusTrapper, createFocusTrapper, FocusTrapperProps, Position } from './createFocusTrapper';
+
+type RefListener = (element: HTMLElement | null) => React.MutableRefObject<HTMLElement | null>;
+
+function useFocusTrapping(props: FocusTrapperProps) {
+  const firstTrapperRef = useRef<HTMLElement | null>(null);
+  const lastTrapperRef = useRef<HTMLElement | null>(null);
+  const tracker = useRef<FocusTrapper | undefined>(undefined);
+
+  const assignElement = (position: Position, element: HTMLElement | null) => {
+    if (!tracker.current) {
+      tracker.current = createFocusTrapper(props);
+    }
+    tracker.current.registerTrackerElement(position, element);
+  };
+  const firstTrapperRefListener: RefListener = useCallback(
+    (observedElement: HTMLElement | null) => {
+      firstTrapperRef.current = observedElement;
+      assignElement('first', observedElement);
+      return firstTrapperRef;
+    },
+    [tracker],
+  );
+  const lastTrapperRefListener: RefListener = useCallback(
+    (observedElement: HTMLElement | null) => {
+      lastTrapperRef.current = observedElement;
+      assignElement('last', observedElement);
+      return lastTrapperRef;
+    },
+    [tracker],
+  );
+
+  const cleanUp = useCallback(() => {
+    if (tracker.current) {
+      tracker.current.dispose();
+      firstTrapperRef.current = null;
+      lastTrapperRef.current = null;
+    }
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      cleanUp();
+    };
+  }, [cleanUp]);
+
+  return {
+    refForFirstTrapper: firstTrapperRefListener,
+    refForLastTrapper: lastTrapperRefListener,
+    disableElements: () => {
+      if (tracker.current) {
+        tracker.current.disableTracking();
+      }
+    },
+    enableElements: () => {
+      if (tracker.current) {
+        tracker.current.enableTracking();
+      }
+    },
+    getElementPosition: (element?: HTMLElement | Node | EventTarget | null): Position | undefined => {
+      if (tracker.current) {
+        return tracker.current.getElementPosition(element);
+      }
+      return undefined;
+    },
+  };
+}
+export default useFocusTrapping;
